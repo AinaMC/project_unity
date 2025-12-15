a94b3dfc53a6038a84188fdee8ffb2474ab4c4da
AinaMC <aina.mestres@estudiantat.upc.edu>
2025-12-15 22:20:11 +0100

    script_collider

---
+++ Assets/Scripts/Tienda_Collider.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Tienda_Collider : MonoBehaviour
+{
+    WorldManagement world;
+    private int puntos_tienda = 10; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnColliderEnter (Collision other)
+    {
+        world.recibir_puntos(10);
+    }
+}
