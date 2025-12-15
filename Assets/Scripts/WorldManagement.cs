95db0a759330adfa59e9a6fddd41d76ccc2df03c
AinaMC <aina.mestres@estudiantat.upc.edu>
2025-12-15 18:32:09 +0100

    CONTADOR FUNCIONA

--- Assets/Scripts/WorldManagement.cs
+++ Assets/Scripts/WorldManagement.cs
@@ -1,27 +1,38 @@
 using UnityEngine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
+
 
 public class WorldManagement : MonoBehaviour
 {
     //Variables
+    //Enviar a script CONTADOR
     Texto_Contador texto_Contador;
-    public Text textoContador;
-
     
     //Trobem el Contador
     void Start()
     {
+        texto_Contador = GetComponent<Texto_Contador>();
         //Actualizamos
-
-        Debug.Log("se ha acrualizado");
-
+        recibir_puntos(0);
     }
-    //Recibir cambios de puntos por interacción
+    //Recibir cambios de puntos por interacción y los mandamos script CONTADOR
     void recibir_puntos(int points)
     {
         texto_Contador.CambiarContador(points);
+        Debug.Log("S'han enviat al Contador: " + points);
+    }
+
+    private void Update()
+    {
+        //Esquerra
+        if(Input.GetMouseButton(0))
+        {
+            texto_Contador.CambiarContador(-1);
+
+        }
+        //Dreta
+        if (Input.GetMouseButton(1))
+        {
+            texto_Contador.CambiarContador(1);
+        }
     }
 }
