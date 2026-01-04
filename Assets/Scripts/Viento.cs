c324fb340cecf574913c346ba5d703a363a04ba7
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-01-05 00:04:01 +0100

    viento aun ni

--- Assets/Scripts/Viento.cs
+++ Assets/Scripts/Viento.cs
@@ -6,16 +6,12 @@ public class Viento : MonoBehaviour
 {
     public player_movement mov;
     public WorldManagement estado;
-
+    public Viento wind;
     //Viento a Favor o en Contra
     //Neutral --> 21-79
     //Utopico (favor) --> 1-20
     //Distopico (en contra) --> 80-100
 
-    void Start()
-    {
-        
-    }
 
     void FixedUpdate()
     {        
@@ -23,28 +19,26 @@ public class Viento : MonoBehaviour
         //var ex = ps.externalForces;
         var main = ps.main;
         var transf = ps.transform;
-   
+
         //Viento
         //Utopico (favor) --> 1-20
         if (estado.estatus_mundo() >= 80 && estado.estatus_mundo() <= 100)
         {
             viento_favor(main);
-            transf.rotation = Quaternion.Euler(0, 180, 0);
+            //transf.rotation = Quaternion.Euler(0, 180, 0);
 
         }
         //Distopico (en contra) --> 80-100
         else if (estado.estatus_mundo() >= 1 && estado.estatus_mundo() <= 20)
         {
             viento_contra(main);
-            transf.rotation = Quaternion.Euler(0, 0, 0);
-
+            //transf.rotation = Quaternion.Euler(0, 0, 0);
         }
         else
         {
             mov.cambiar_vel(7f);
-            transf.rotation = Quaternion.Euler(0, 180, 0);
+            //transf.rotation = Quaternion.Euler(0, 180, 0);
             main.startLifetime = 0.0001f;
-
         }
     }
 
