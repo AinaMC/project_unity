fe3f1b2e3ff08d367152d5cdf0160000859678c3
ArnauMorenoGrau <arnau.moreno.grau@estudiantat.upc.edu>
2026-01-09 01:58:08 +0100

    prueva skydome changing

---
+++ Assets/Sources/Locations/Time light/Time Manager.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D skyboxNight;
+    //[SerializeField] private Texture2D skyboxSunrise;
+    [SerializeField] private Texture2D skyboxDay;
+    //[SerializeField] private Texture2D skyboxSunset;
+
+    [SerializeField] private Gradient graddientNightToDay;
+    //[SerializeField] private Gradient graddientSunriseToDay;
+    //[SerializeField] private Gradient graddientDayToSunset;
+    [SerializeField] private Gradient graddientDayToNight;
+
+    [SerializeField] private Light globalLight;
+
+    private int minutes;
+
+    public int Minutes
+    { get { return minutes; } set { minutes = value; OnMinutesChange(value); } }
+
+    private int hours = 10;
+
+    public int Hours
+    { get { return hours; } set { hours = value; OnHoursChange(value); } }
+
+    private int days;
+
+    public int Days
+    { get { return days; } set { days = value; } }
+
+    private float tempSecond;
+
+    public void Update()
+    {
+        tempSecond += Time.deltaTime;
+
+        if (tempSecond >= 1)
+        {
+            Minutes += 10;
+            tempSecond = 0;
+        }
+    }
+
+    private void OnMinutesChange(int value)
+    {
+        globalLight.transform.Rotate(Vector3.left, (1f / (1440f / 4f)) * 360f, Space.World);
+        if (value >= 10)
+        {
+            Hours++;
+            minutes = 0;
+        }
+        if (Hours >= 24)
+        {
+            Hours = 0;
+            Days++;
+        }
+    }
+
+    private void OnHoursChange(int value)
+    {
+        if (value == 6)
+        {
+            StartCoroutine(LerpSkybox(skyboxNight, skyboxDay, 1f));
+            StartCoroutine(LerpLight(graddientNightToDay, 1f));
+        }
+        //else if (value == 8)
+        //{
+        //    StartCoroutine(LerpSkybox(skyboxNight, skyboxDay, 10f));
+        //    StartCoroutine(LerpLight(graddientSunriseToDay, 10f));
+        //}
+        //else if (value == 18)
+        //{
+        //    StartCoroutine(LerpSkybox(skyboxDay, skyboxSunset, 10f));
+        //    StartCoroutine(LerpLight(graddientDayToSunset, 10f));
+        //}
+        else if (value == 22)
+        {
+            StartCoroutine(LerpSkybox(skyboxDay, skyboxNight, 1f));
+            StartCoroutine(LerpLight(graddientDayToNight, 1f));
+        }
+    }
+
+    private IEnumerator LerpSkybox(Texture2D a, Texture2D b, float time)
+    {
+        RenderSettings.skybox.SetTexture("_Texture1", a);
+        RenderSettings.skybox.SetTexture("_Texture2", b);
+        RenderSettings.skybox.SetFloat("_Blend", 0);
+        for (float i = 0; i < time; i += Time.deltaTime)
+        {
+            RenderSettings.skybox.SetFloat("_Blend", i / time);
+            yield return null;
+        }
+        RenderSettings.skybox.SetTexture("_Texture1", b);
+    }
+
+    private IEnumerator LerpLight(Gradient lightGradient, float time)
+    {
+        for (float i = 0; i < time; i += Time.deltaTime)
+        {
+            globalLight.color = lightGradient.Evaluate(i / time);
+            RenderSettings.fogColor = globalLight.color;
+            yield return null;
+        }
+    }
+}
+
